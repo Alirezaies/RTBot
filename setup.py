fc342d5ba6af232badabc20cf855319b6ebaabc0
AlirezaieS <alirezaie@sadegh.io>
2019-05-31 15:19:59 +0430

    init

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 import setuptools
+import platform
+
+os_type = platform.system()
+
+# check the platform and raise an error if OS is not Linux.
+if os_type != 'Linux':
+    raise OSError('Error: This Package Can Only Be Installed On Linux.')
 
 with open('README.md', 'r') as fh:
     long_desc = fh.read()
 
+
+
 setuptools.setup(
     name='RTBot',
     version='0.0.0',
-    scripts=['app'],
+    scripts=['RadioTonight'],
     author='Mohammad Sadegh Alirezaie',
     author_email='alirezaie@sadegh.io',
-    author_website='https://sadegh.io',
     description='Radio Tonight Bot',
     long_description=long_desc,
     long_description_content_type='text/markdown',
