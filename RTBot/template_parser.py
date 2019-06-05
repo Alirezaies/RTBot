6b596e143b5b17ca856a9e19aec58174646718ce
AlirezaieS <alirezaie@sadegh.io>
2019-06-05 16:28:30 +0430

    message templating system

---
+++ RTBot/template_parser.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+
+class template_parser:
+    def __init__(self) -> None:
+        template = (os.path.dirname(
+            os.path.realpath(__file__)), 'template.json')
+        template = '/config/'.join(template)
+
+        with open(template, 'r') as tmp:
+            self.template = json.load(tmp)
+
+    def deploy_message_parser(
+        self,
+        song_number: str,
+        song_name: str,
+        song_artist: str
+    ) -> str:
+        """ format the music deployment message """
+
+        message = '\n'.join(self.template['music']['deploy_message'].format(
+            song_number, song_name, song_artist))
+
+        return(message)
